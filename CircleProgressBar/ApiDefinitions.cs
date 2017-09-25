88a5efdcf1c62d149671cea36dd49ff8a04fe5dc
Aktar Ibrahim <aktar@ikomobi.com>
2017-09-25 15:27:45 +0200

    Added x386 x86_64 architecture support to the fat lib
    also
    - re-generated the ApiDefinition file with Sharpie 3.3
    - Removed an unnecessary config "Release-China"
    - Updated Nuget

--- CircleProgressBar/ApiDefinitions.cs
+++ CircleProgressBar/ApiDefinitions.cs
@@ -1,5 +1,4 @@
 using System;
-
 using Foundation;
 using ObjCRuntime;
 using UIKit;
@@ -7,77 +6,77 @@ using UIKit;
 namespace CustomProgressBar
 {
     // typedef NSString * (^StringGenerationBlock)(CGFloat);
-    delegate NSString StringGenerationBlock (nfloat arg0);
+    delegate string StringGenerationBlock(nfloat arg0);
 
     // typedef NSAttributedString * (^AttributedStringGenerationBlock)(CGFloat);
-    delegate NSAttributedString AttributedStringGenerationBlock (nfloat arg0);
+    delegate NSAttributedString AttributedStringGenerationBlock(nfloat arg0);
 
     // @interface CircleProgressBar : UIView
     [BaseType(typeof(UIView))]
     interface CircleProgressBar
     {
         // @property (nonatomic) CGFloat progressBarWidth;
-        [Export ("progressBarWidth")]
+        [Export("progressBarWidth")]
         nfloat ProgressBarWidth { get; set; }
 
         // @property (nonatomic) UIColor * progressBarProgressColor;
-        [Export ("progressBarProgressColor", ArgumentSemantic.Assign)]
+        [Export("progressBarProgressColor", ArgumentSemantic.Assign)]
         UIColor ProgressBarProgressColor { get; set; }
 
         // @property (nonatomic) UIColor * progressBarTrackColor;
-        [Export ("progressBarTrackColor", ArgumentSemantic.Assign)]
+        [Export("progressBarTrackColor", ArgumentSemantic.Assign)]
         UIColor ProgressBarTrackColor { get; set; }
 
         // @property (nonatomic) CGFloat startAngle;
-        [Export ("startAngle")]
+        [Export("startAngle")]
         nfloat StartAngle { get; set; }
 
         // @property (nonatomic) BOOL hintHidden;
-        [Export ("hintHidden")]
+        [Export("hintHidden")]
         bool HintHidden { get; set; }
 
         // @property (nonatomic) CGFloat hintViewSpacing;
-        [Export ("hintViewSpacing")]
+        [Export("hintViewSpacing")]
         nfloat HintViewSpacing { get; set; }
 
         // @property (nonatomic) UIColor * hintViewBackgroundColor;
-        [Export ("hintViewBackgroundColor", ArgumentSemantic.Assign)]
+        [Export("hintViewBackgroundColor", ArgumentSemantic.Assign)]
         UIColor HintViewBackgroundColor { get; set; }
 
         // @property (nonatomic) UIFont * hintTextFont;
-        [Export ("hintTextFont", ArgumentSemantic.Assign)]
+        [Export("hintTextFont", ArgumentSemantic.Assign)]
         UIFont HintTextFont { get; set; }
 
         // @property (nonatomic) UIColor * hintTextColor;
-        [Export ("hintTextColor", ArgumentSemantic.Assign)]
+        [Export("hintTextColor", ArgumentSemantic.Assign)]
         UIColor HintTextColor { get; set; }
 
         // -(void)setHintTextGenerationBlock:(StringGenerationBlock)generationBlock;
-        [Export ("setHintTextGenerationBlock:")]
-        void SetHintTextGenerationBlock (StringGenerationBlock generationBlock);
+        [Export("setHintTextGenerationBlock:")]
+        void SetHintTextGenerationBlock(StringGenerationBlock generationBlock);
 
         // -(void)setHintAttributedGenerationBlock:(AttributedStringGenerationBlock)generationBlock;
-        [Export ("setHintAttributedGenerationBlock:")]
-        void SetHintAttributedGenerationBlock (AttributedStringGenerationBlock generationBlock);
+        [Export("setHintAttributedGenerationBlock:")]
+        void SetHintAttributedGenerationBlock(AttributedStringGenerationBlock generationBlock);
 
         // @property (readonly, nonatomic) CGFloat progress;
-        [Export ("progress")]
+        [Export("progress")]
         nfloat Progress { get; }
 
         // @property (readonly, nonatomic) BOOL isAnimating;
-        [Export ("isAnimating")]
+        [Export("isAnimating")]
         bool IsAnimating { get; }
 
         // -(void)setProgress:(CGFloat)progress animated:(BOOL)animated;
-        [Export ("setProgress:animated:")]
-        void SetProgress (nfloat progress, bool animated);
+        [Export("setProgress:animated:")]
+        void SetProgress(nfloat progress, bool animated);
 
         // -(void)setProgress:(CGFloat)progress animated:(BOOL)animated duration:(CGFloat)duration;
-        [Export ("setProgress:animated:duration:")]
-        void SetProgress (nfloat progress, bool animated, nfloat duration);
+        [Export("setProgress:animated:duration:")]
+        void SetProgress(nfloat progress, bool animated, nfloat duration);
 
         // -(void)stopAnimation;
-        [Export ("stopAnimation")]
-        void StopAnimation ();
+        [Export("stopAnimation")]
+        void StopAnimation();
     }
 }
